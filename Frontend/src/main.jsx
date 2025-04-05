80ba62072bf587463c34de7f9605482cab11c8ff
Abhishek83gupta <abhi12345678gupta@gmail.com>
2025-04-05 14:17:56 +0530

    basic setup done

---
+++ Frontend/src/main.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Layout from './components/Layout'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+import Home from "./components/Home";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>,
+      },
+      {
+        path: "/test",
+        element: <h1>Test</h1>,
+      }
+    ],
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
